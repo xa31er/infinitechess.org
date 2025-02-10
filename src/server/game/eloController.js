a919ee6cfb1a1c8178a0ce61fe466c1d742489f7
xa31er <condexabier@gmail.com>
2025-02-10 12:58:56 +0100

    Added determineNewRD function

--- src/server/game/eloController.js
+++ src/server/game/eloController.js
@@ -1,5 +1,12 @@
 /*
- * Implementation on fthe glicko rating system.
- */
+	* Implementation on fthe glicko rating system.
+	*/
 
-function determineNewRD()
+function determineNewRD(OldRD, c) {
+	const oldRDSquared = Math.pow(OldRD, 2);
+	const cSquared = Math.pow(c, 2);
+
+	const newRD = Math.min(Math.sqrt(oldRDSquared + cSquared), 350);
+
+	return newRD;
+}
